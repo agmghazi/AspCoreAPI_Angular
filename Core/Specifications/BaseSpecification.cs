6cb7fee4d24546551ce7d3388ed7cce2c11da77b
Ahmed Gamal <gis.csharp@gmail.com>
2021-03-27 18:08:13 +0300

    end of section 4

---
+++ Core/Specifications/BaseSpecification.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Core.Specifications
+{
+    public class BaseSpecification<T> : ISpecification<T>
+    {
+        public BaseSpecification()
+        {
+        }
+
+        public BaseSpecification(Expression<Func<T, bool>> criterial)
+        {
+            Criterial = criterial;
+        }
+
+        public Expression<Func<T, bool>> Criterial { get; }
+        public List<Expression<Func<T, object>>> Includes { get; } =
+              new List<Expression<Func<T, object>>>();
+
+        protected void AddInclude(Expression<Func<T, object>> includeExpression)
+        {
+            Includes.Add(includeExpression);
+        }
+    }
+}
